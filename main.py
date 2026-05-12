0d6ea14f01152bb3b96427457c11de46e35c49fd
Iman Osmic <iman.osmic@fet.ba>
2026-05-12 22:53:48 +0200

    Povezi rute za termine sa aplikacijom

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 
 from database import create_db_and_tables
 from routes_a import router as patients_router
+from routes_b import router as appointments_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -18,6 +19,8 @@ app = FastAPI(
 
 # Registracija routera Studenta A (pacijenti)
 app.include_router(patients_router)
+# Registracija routera Studenta B (termini pregleda)
+app.include_router(appointments_router)
 
 @app.get("/")
 def read_root():
